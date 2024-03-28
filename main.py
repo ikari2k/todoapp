e59e647e108de86c0fd4b82d3a1c39b8f5358ddc
ikari2k <pluta.m@gmail.com>
2024-03-28 15:16:02 +0100

    added health check endpoint and first test against it

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from routers import auth, todos, admin, users
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
+@app.get("/healthy")
+def health_check():
+    return {"status": "Healthy"}
+
+
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
