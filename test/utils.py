270e489a5cfc4c8ce35ea8a7ce316116a4485ce1
ikari2k <pluta.m@gmail.com>
2024-03-29 21:45:39 +0100

    extract repeating code into utils

---
+++ test/utils.py
@@ -0,0 +1,56 @@
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, text
+
+from fastapi.testclient import TestClient
+
+from database import Base
+import pytest
+
+from main import app
+from models import Todos
+
+SQLALCHEMY_DB_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(
+    SQLALCHEMY_DB_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def override_get_current_user():
+    return {"username": "admin", "id": 1, "user_role": "admin"}
+
+
+client = TestClient(app)
+
+
+@pytest.fixture
+def test_todo():
+    todo = Todos(
+        title="Learn to code",
+        description="Need to learn everyday",
+        priority=5,
+        complete=False,
+        owner_id=1,
+    )
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as conn:
+        conn.execute(text("DELETE FROM todos;"))
+        conn.commit()
